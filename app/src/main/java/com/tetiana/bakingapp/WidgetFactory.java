3e3796c29d8daf5f3b617c8a94ce064a40181d22
Tetiana Horobets <tanusha.sts@gmail.com>
2018-05-09 16:51:43 +0200

    refactor

---
+++ app/src/main/java/com/tetiana/bakingapp/WidgetFactory.java
@@ -0,0 +1,95 @@
+package com.tetiana.bakingapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.tetiana.bakingapp.model.Ingredient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class WidgetFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private static final int ID_CONSTANT = 0x0101010;
+
+    private ArrayList<Ingredient> ingredients;
+    private Context mContext;
+
+    public WidgetFactory(Context context, Intent intent) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate() {
+        ingredients = new ArrayList<Ingredient>();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        int id = preferences.getInt("recipeID", 0);
+        try {
+            ReadData readData = new ReadData(mContext);
+            ingredients = readData.getRecipe(id).getIngredients();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return ingredients.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return ID_CONSTANT + position;
+    }
+
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public RemoteViews getViewAt(int position) {
+
+        Ingredient article = ingredients.get(position);
+
+        RemoteViews itemView = new RemoteViews(mContext.getPackageName(), R.layout.ingredient_list_item);
+
+        itemView.setTextViewText(R.id.measure, article.getMeasure());
+        itemView.setTextViewText(R.id.quantity, article.getQuantity());
+        itemView.setTextViewText(R.id.ingredient, article.getIngredientName());
+
+        Intent intent = new Intent();
+        intent.putExtra(WidgetProvider.EXTRA_ITEM, article);
+        itemView.setOnClickFillInIntent(R.id.lvList, intent);
+
+        return itemView;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        ingredients.clear();
+    }
+}
