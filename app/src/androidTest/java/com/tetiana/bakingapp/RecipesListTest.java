d582a0e82e8decfa39c6c6c9218eb05158f53d05
Tetiana Horobets <tanusha.sts@gmail.com>
2018-05-19 15:11:36 +0200

    add test

--- app/src/androidTest/java/com/tetiana/bakingapp/RecipesListTest.java
+++ app/src/androidTest/java/com/tetiana/bakingapp/RecipesListTest.java
@@ -1,7 +1,5 @@
 package com.tetiana.bakingapp;
 
-import android.os.SystemClock;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -12,28 +10,25 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class RecipesListTest {
-//        @Rule
-//    public ActivityTestRule<MainActivity> mActivityRule =
-//            new ActivityTestRule(MainActivity.class);
-//
-//        @Test
-//        public void clickRecyclerViewItem_PassesRecipeNameToNextActivity() {
-//            try {
-//                Thread.sleep(5000); //wait 5 seconds before going on
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            onView(withId(R.id.rv_show_recipes_list).perform(RecyclerViewActions
-//                    .actionOnItemAtPosition(1, click()));
-//            onView(withId(R.id.recipe_title)).check(matches(withText("Nutella Pie")));
-//        }
+        @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule(MainActivity.class);
 
-
-
+        @Test
+        public void clickRecyclerViewItem_PassesRecipeNameToNextActivity() {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            onView(withId(R.id.rv_show_recipes_list))
+                .perform(actionOnItemAtPosition(0, click()));
+            onView(withId(R.id.rv_step_list)).check(matches(isDisplayed()));
+        }
 }
--- app/src/androidTest/java/com/tetiana/bakingapp/StepDetailTest.java
+++ app/src/androidTest/java/com/tetiana/bakingapp/StepDetailTest.java
@@ -15,11 +15,12 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
-public class IngredientTest {
+public class StepDetailTest {
     @Rule
     public ActivityTestRule<StepActivity> mActivityRule =
             new ActivityTestRule<StepActivity>(StepActivity.class) {
@@ -28,22 +29,20 @@ public class IngredientTest {
                     Context targetContext = InstrumentationRegistry.getInstrumentation()
                             .getTargetContext();
                     Intent result = new Intent(targetContext, StepActivity.class);
-                    result.putExtra("recipeID", 1);
+                    result.putExtra("stepID", 1);
                     return result;
                 }
             };
 
     @Test
-    public void clickIngredientButton_OpenNewActivity(){
+    public void clickRecyclerViewItem_OpenDetail() {
         try {
-            Thread.sleep(5000); //wait 5 seconds before going on
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        onView(withId(R.id.ingredients))
-                .perform(click());
-
-        onView(withId(R.id.rv_ingredient_list)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_step_list))
+                .perform(actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.frame_layout_step)).check(matches(isDisplayed()));
     }
 }
